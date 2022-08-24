c32f18952d3f7a8d379ea7f85a7b7cc73b52292e
JPampo <108749131+JPampo@users.noreply.github.com>
2022-08-24 14:36:43 -0300

    Função para criar valores
    insere_valores_no_vetor

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TAM_VETOR 10
+
+/*
+ * Nome: insere_valores_no_vetor
+ * Parametros: Vetor de inteiros
+ * Retorno: nada
+ */
+void insere_valores_no_vetor(int vet[])
+{
+    for(int i = 0; i < TAM_VETOR; i++)
+    {
+        vet[i] = rand() % 5000;
+    }
+}
+
+
 int main()
 {
     printf("ZA WARUDO: \n\n");
