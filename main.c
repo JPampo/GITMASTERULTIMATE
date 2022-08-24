d39ee3dc1620a66b33a3bacc1b492db33c55366a
JPampo <108749131+JPampo@users.noreply.github.com>
2022-08-24 14:38:46 -0300

    Função de impressão
    imprimir_vetor

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TAM_VETOR 10
+
+void imprimir_vetor(int vet[])
+{
+    for(int i = 0; i < TAM_VETOR; i++)
+        printf("[%d] - %d\n", i, vet[i]);
+}
+
+
 int main()
 {
     printf("ZA WARUDO: \n\n");
