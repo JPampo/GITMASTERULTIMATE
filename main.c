2a8c4c5488324cccb82d7698673bf5605fc99e29
Yamakasuki <111364032+Yamakasuki@users.noreply.github.com>
2022-08-26 08:31:39 -0300

    inserindo mediana na brach
    futura implementação do develop nessa branch

--- main.c
+++ main.c
@@ -15,19 +15,25 @@ void insere_valores_no_vetor(int vet[])
     {
         vet[i] = rand() % 5000;
     }
-
+}
 void imprimir_vetor(int vet[])
 {
-    for(int i = 0; i < TAM_VETOR; i++)
+    for(int i = 0; i < TAM_VETOR; i++){
         printf("[%d] - %d\n", i, vet[i]);
+    }
 
 }
+void mediana_vetor(int vet[]){
+    float mediana;
+     mediana = ((float)vet[500]+ (float)vet[499])/2;
+     printf("%.2f", mediana);
 
+}
 
 int main()
 {
     printf("ZA WARUDO: \n\n");
     int v[1000];
-        
+    mediana_vetor(v);
     return 0;
 }
