3e2f370d7eb79de0ef87bca904cf28c333a1f3af
Yamakasuki <111364032+Yamakasuki@users.noreply.github.com>
2022-08-26 08:20:05 -0300

    Inserindo função da média
    media

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TAM_VETOR 100
+#define TAM_VETOR 1000
 
 
 /*
@@ -14,6 +14,15 @@ void insere_valores_no_vetor(int vet[])
     for(int i = 0; i < TAM_VETOR; i++)
     {
         vet[i] = rand() % 5000;
+    }
+}
+void media_vetor(int vet[]){
+    float soma=0,media;
+    for(int i=0;i<TAM_VETOR;i++){
+        soma=vet[i]+soma;
+    }
+    media=soma/TAM_VETOR;
+    printf("Valor da media: %f",media);
     }
 
 void imprimir_vetor(int vet[])
@@ -30,6 +39,7 @@ int main()
     int v[1000];
     insere_valores_no_vetor(v);
     imprimir_vetor(v);
+    media_vetor(v);
         
     return 0;
 }
